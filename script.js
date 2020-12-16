36ee4ba676423b0e7877062c2945e39e7b710486
Igor Torgaiev <Itorgaiev@gmail.com>
2020-12-16 12:54:09 +0100

    My first portfolio composed of: HTML5, CSS3 and Bootstrap

---
+++ script.js
@@ -0,0 +1,23 @@
+// $(document).ready(function() {
+//     $("h1").hover(function() {
+//         $("#portfolio").slideUp(1000);
+//     });
+//     $("h1").hover(function() {
+//         $("#portfolio").slideDown(1000);
+//     });
+// });
+
+// $(document).ready(function() {
+//     $("div").click(function() {
+//         $(".description").show(2000);
+//     });
+// });
+
+$(document).ready(function() {
+    $("h1").hover(function(){
+        $(".portfolio").slideUp(2000);
+    });
+    $("h1").hover(function(){
+        $(".portfolio").slideDown(2000);
+    });
+});
